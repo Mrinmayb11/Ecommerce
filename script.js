2c4f6f40f52bb8092461950cd803ea35cb258ca2
Mrinmayb11 <mrinmaybiswas119@gmail.com>
2023-05-11 17:25:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const Bars = document.getElementById("Bars");
+
+Bars.addEventListener("click",()=>{
+    const SmallNav = document.getElementById("Nav-3");
+    SmallNav.classList.toggle("add");
+})
