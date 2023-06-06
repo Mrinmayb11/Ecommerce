676576b7e2078f4d4206a1e4aee0dfc5ade450aa
Mrinmayb11 <mrinmaybiswas119@gmail.com>
2023-06-06 15:04:22 +0530

    first commit

--- script.js
+++ script.js
@@ -5,4 +5,16 @@ Bars.addEventListener("click",()=>{
     SmallNav.classList.toggle("add");
 })
 
+// New Arrivials Section
+
+// const Imgs = document.getElementsByClassName("feature-img-wrapper");
+// Array.from(Imgs).forEach(img =>{
+//     img.addEventListener('mouseover',  ()=>{
+//         const btns = img.lastElementChild.children;
+//         for (let i = 0; i < btns.length; i++) {
+//             btns[i].style.display = "flex";
+//           }
+//     })
+
+// })
 
